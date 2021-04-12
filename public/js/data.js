1c01bcfdfa29d542cd4646ab84828a8cb02aba14
dipranjanJS <dipranjan16oct@gmail.com>
2021-04-12 22:25:31 +0530

    add about me section

--- public/js/data.js
+++ public/js/data.js
@@ -17,7 +17,24 @@ const projects = [
         text: 'Feeling bored!! How about some good music to change your mood? Click the lick and see for yourself.',
         link: 'https://dipranjanjs.github.io/music-player/'
     },
-    
+    {
+        name: 'Drum practice',
+        img: '/images/drum-kit.png',
+        text: 'Want to play drums but not having the kit? No worries. Click on the button and start playing drums. Decide whether you want to start a new career or not with it.',
+        link: 'https://dipranjanjs.github.io/drum-kit/'
+    },
+    {
+        name: 'Simon',
+        img: '/images/simon-game.png',
+        text: 'You think your memory is sharp? Then challenge yourself with the all time favourite simon game and push your memory further.',
+        link: 'https://dipranjanjs.github.io/simon-game/'
+    },
+    {
+        name: 'Fire detector',
+        img: '/images/wild-fire.png',
+        text: 'Get a world-wide view of all the recent wild fires with their location.Get updated of the wild fires that took place near you or any other places on the globe',
+        link: 'https://wild-fire-update.herokuapp.com/'
+    }
 ];
 
 module.exports = projects;
